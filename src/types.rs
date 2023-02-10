d7854a12f33f15ca50dd64f6096c337704c32d68
Velnbur <kyrylo.baybula@distributedlab.com>
2023-02-10 13:25:09 +0200

    Add participant and rooms

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,5 @@
 use ethers::types::{Address, Signature, U256};
+use rsa::RsaPublicKey;
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Clone)]
@@ -6,6 +7,20 @@ pub struct Room {
     pub id: uuid::Uuid,
     pub token: Address,
     pub amount: U256,
+    pub current_round: u64,
+    pub participants: Vec<Participant>,
+}
+
+impl Room {
+    pub fn new(token: Address, amount: U256) -> Self {
+        Self {
+            id: uuid::Uuid::new_v4(),
+            token,
+            amount,
+            current_round: 0,
+            participants: Vec::new(),
+        }
+    }
 }
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -24,11 +39,24 @@ pub struct Participant {
     pub id: uuid::Uuid,
     pub room_id: uuid::Uuid,
     pub utxo_id: U256,
+    pub rsa_pubkey: RsaPublicKey,
     pub state: ShuffleState,
 }
 
+impl Participant {
+    pub fn new(room_id: uuid::Uuid, utxo_id: U256, pubkey: RsaPublicKey) -> Self {
+        Self {
+            id: uuid::Uuid::new_v4(),
+            room_id,
+            utxo_id,
+            rsa_pubkey: pubkey,
+            state: ShuffleState::Start,
+        }
+    }
+}
+
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Input {
     pub id: U256,
     pub signature: Signature,
