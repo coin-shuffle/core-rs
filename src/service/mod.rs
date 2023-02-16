958bea5506bf6b3a42c9ab86e77014dc71b04732
Velnbur <kyrylo.baybula@distributedlab.com>
2023-02-16 13:13:01 +0200

    Update service

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,11 +1,14 @@
 pub mod storage;
+#[cfg(test)]
+mod tests;
 pub mod types;
 pub mod waiter;
 
 use ethers_core::types::{Address, U256};
+use rsa::RsaPublicKey;
 
 use self::{
-    storage::{participants, queues, rooms},
+    storage::participants,
     types::{participant::Participant, room::Room},
 };
 
@@ -27,41 +30,109 @@ where
         Self { storage, waiter }
     }
 
+    /// Add participant to the queue for given token and amount.
     pub async fn add_participant(
         &self,
         token: &Address,
         amount: &U256,
         participant: &Participant,
-    ) -> Result<(), Error<W::Error, <S as queues::Storage>::Error>> {
+    ) -> Result<(), AddParticipantError<W::InternalError, <S as storage::Storage>::InternalError>>
+    {
+        self.storage
+            .insert_participants(vec![participant.clone()]) // TODO:
+            .await?;
+
         self.waiter
             .add_to_queue(token, amount, &participant.utxo_id)
             .await
-            .map_err(Error::Waiter)
+            .map_err(AddParticipantError::Waiter)?;
+
+        Ok(())
     }
 
-    pub async fn start_shuffle(
+    /// Remove participants from the queue for given token and amount
+    /// and form rooms for all of them. Return id's of newly created rooms.
+    pub async fn create_rooms(
         &self,
         token: &Address,
         amount: &U256,
-    ) -> Result<(), Error<W::Error, <S as rooms::Storage>::Error>> {
-        let rooms = self
-            .waiter
-            .organize(token, amount)
-            .await
-            .map_err(Error::Waiter)?;
+    ) -> Result<Vec<uuid::Uuid>, CreateRoomsError<W::InternalError>> {
+        let rooms = self.waiter.organize(token, amount).await?;
 
-        Ok(())
+        Ok(rooms)
+    }
+
+    /// Start shuffling in the room. Return pairs of participant id and array of
+    /// RSA public keys needed to encrypt messages.
+    pub async fn start_shuffle(
+        &self,
+        room_id: &uuid::Uuid,
+    ) -> Result<
+        Vec<(U256, Vec<RsaPublicKey>)>,
+        ShuffleRoundError<<S as storage::Storage>::InternalError>,
+    > {
+        let room = self
+            .storage
+            .get_room(room_id)
+            .await
+            .map_err(ShuffleRoundError::RoomStorage)?
+            .ok_or(ShuffleRoundError::RoomNotFound)?;
+
+        let participants_number = room.participants.len();
+
+        let mut result = Vec::with_capacity(participants_number);
+        let mut keys = Vec::with_capacity(participants_number);
+
+        for participant in room.participants.iter().rev() {
+            let key = self
+                .storage
+                .get_participant(&participant)
+                .await
+                .map_err(ShuffleRoundError::ParticipantStorage)?
+                .ok_or(ShuffleRoundError::ParticipantNotFound)?
+                .rsa_pubkey;
+
+            keys.push(key);
+
+            result.push((participant.clone(), keys.clone()))
+        }
+
+        Ok(result)
     }
 }
 
 #[derive(thiserror::Error, Debug)]
-pub enum Error<WE, SE>
+pub enum AddParticipantError<WE, DE>
 where
     WE: std::error::Error,
+    DE: std::error::Error,
+{
+    #[error("failed to insert participant: {0}")]
+    InsertParticipant(#[from] participants::InsertError<DE>),
+    #[error("failed to add paritipant to queue: {0}")]
+    Waiter(WE),
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum CreateRoomsError<WE>
+where
+    WE: std::error::Error,
+{
+    #[error("failed to organize rooms: {0}")]
+    Waiter(#[from] WE),
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum ShuffleRoundError<SE>
+where
     SE: std::error::Error,
 {
-    #[error("Waiter error: {0}")]
-    Waiter(WE),
-    #[error("Storage error: {0}")]
-    Storage(SE),
+    #[error("failed to get room: {0}")]
+    RoomStorage(SE),
+    #[error("room no found")]
+    RoomNotFound,
+    #[error("failed to get participant: {0}")]
+    ParticipantStorage(SE),
+    #[error("participant no found")]
+    ParticipantNotFound,
 }
