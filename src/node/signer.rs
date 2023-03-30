7b1e6bddaebab88f581c7aecc2130d112af72683
Velnbur <kyrylo.baybula@distributedlab.com>
2023-03-30 19:41:37 +0300

    `Signer` should return `Signature` instead of bytes

--- src/node/signer.rs
+++ src/node/signer.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use ethers_core::types::Signature;
 
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
@@ -8,5 +9,5 @@ pub trait Signer {
     async fn sign_message<S: Send + Sync + AsRef<[u8]>>(
         &self,
         message: S,
-    ) -> Result<Vec<u8>, Self::Error>;
+    ) -> Result<Signature, Self::Error>;
 }
